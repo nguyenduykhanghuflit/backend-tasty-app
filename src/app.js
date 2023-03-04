9f82fa154b4165e56d7b0fb37fe3f81fede3b5f8
NguyenDuyKhang <nguyenduykhanghuflit@gmail.com>
2023-03-04 13:41:24 +0700

    connect db

--- src/app.js
+++ src/app.js
@@ -1,45 +1,58 @@
 const express = require('express');
 const app = express();
-const helmet = require('helmet')
-const morgan = require('morgan')
-const compression = require('compression')
+const helmet = require('helmet');
+const morgan = require('morgan');
+const compression = require('compression');
+const cors = require('cors');
 
-//init dbs 
-// require('./v1/databases/init.mongodb')
-// require('./v1/databases/init.redis')
+const databaseLoad = require('./v1/databases/init.mysql');
+
+//init dbs
+databaseLoad();
 
 //user middleware
-app.use(helmet())
-app.use(morgan('combined'))
+app.use(helmet());
+app.use(morgan('combined'));
+
 // compress responses
-app.use(compression())
+app.use(compression());
 
 // add body-parser
-app.use(express.json())
-app.use(express.urlencoded({
-    extended: true
-}))
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(
+  morgan(
+    ':method :url :status :res[content-length] - :response-time ms  :date[clf]'
+  )
+);
+app.use(cors({ credentials: true, origin: true }));
+// app.use(cookieParser(process.env.signed_cookie));
+//app.use('/static/', express.static(path.join(__dirname, '../api/public')));
 
 //router
-app.use(require('./v1/routes/index.router'))
+app.use(require('./v1/routes/index.router'));
 
-// Error Handling Middleware called
+// Error Handling Middleware
 
 app.use((req, res, next) => {
-    const error = new Error("Not found");
-    error.status = 404;
-    next(error);
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
 });
-
 
 // error handler middleware
 app.use((error, req, res, next) => {
-    res.status(error.status || 500).send({
-        error: {
-            status: error.status || 500,
-            message: error.message || 'Internal Server Error',
-        },
-    });
+  res.status(error.status || 500).send({
+    error: {
+      status: error.status || 500,
+      message: error.message || 'Internal Server Error',
+    },
+  });
 });
 
 module.exports = app;
---
+++ src/v1/databases/init.mysql.js
@@ -0,0 +1,35 @@
+const Sequelize = require('sequelize');
+
+const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+  host: DB_HOST,
+  dialect: 'mysql',
+
+  pool: {
+    max: 5,
+    min: 0,
+  },
+  define: {
+    charset: 'utf8mb4',
+    collate: 'utf8mb4_general_ci',
+    underscored: true,
+    freezeTableName: true,
+  },
+
+  benchmark: false,
+  logger: false,
+  //   logging: (sql, timingMs) =>
+  //     logger.info(`${sql} - [Execution time: ${timingMs}ms]`),
+});
+
+const connectDB = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Kết nối CSDL thành công ');
+  } catch (error) {
+    console.error('Kết nối CSDL thất bại:', error);
+  }
+};
+
+module.exports = connectDB;
